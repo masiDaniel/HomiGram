8f8e6111dbc817f0851c504ec6bb7c6d4c3260aa
masiDaniel <masidaniel02@gmail.com>
2024-08-22 18:42:41 +0300

    cleaning the project structure for deployment

--- HomieGramBackEnd/HomieGramBackEnd/houses/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import LandLords, Houses, CareTaker, Teenants 
-
-# Register your models here.
-models_to_register = [LandLords, Houses, CareTaker, Teenants]
-admin.site.register(models_to_register)
